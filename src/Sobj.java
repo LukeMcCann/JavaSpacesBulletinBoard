93b9b58be69c591ac8870a27c5ec02de307103cb
LukeMcCann <developer-lukemccann@outlook.com>
2018-11-17 19:46:59 +0000

    Fixes missing interface

--- src/Sobj.java
+++ src/Sobj.java
@@ -1,4 +1,16 @@
-package PACKAGE_NAME;
+import net.jini.core.entry.*;
 
-public class Sobj {
+public class Sobj implements Entry
+{
+    public String contents;
+
+    /*
+     * Sobj - no arg constructor
+     * required for functionality
+     */
+    public Sobj() {}
+
+    public Sobj(String s) {
+        contents = s;
+    }
 }
